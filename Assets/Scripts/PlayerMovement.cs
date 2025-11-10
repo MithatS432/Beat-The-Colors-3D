0fa5874be688e93a5fde32c4625452da34c25351
Mithat <mithatsirmali@gmail.com>
2025-11-10 14:20:46 +0300

    Player script settings

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,76 @@
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
-using UnityEngine.InputSystem;
-#endif
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Components")]
+    private Rigidbody rb;
+    private AudioSource audioSource;
+
     [Header("Camera Settings")]
     public Camera playerCamera;
     public Camera secondaryCamera;
     public bool isUsingSecondaryCamera = false;
+
+    [Header("UI Settings")]
+    public Button pauseButton;
+    public Button continueButton;
+    public Button exitButton;
+
+    [Header("Character Settings")]
+    private int score = 0;
+    public int waveCount = 1;
+    private int health = 3;
+    public int ballCount = 5;
+    public string ballColor = "Red";
+    public TMP_Text scoreText;
+    public TMP_Text waveText;
+    public TMP_Text healthText;
+    public TMP_Text ballCountText;
+    public TMP_Text ballColorText;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpForce = 7f;
+    private bool isGrounded;
+    private float rotateSpeed = 100f;
+
     void Start()
     {
-        if (isUsingSecondaryCamera)
-        {
-            playerCamera.enabled = false;
-            secondaryCamera.enabled = true;
-        }
-        else
-        {
-            playerCamera.enabled = true;
-            secondaryCamera.enabled = false;
-        }
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        playerCamera.enabled = !isUsingSecondaryCamera;
+        secondaryCamera.enabled = isUsingSecondaryCamera;
     }
 
     void Update()
     {
-#if ENABLE_LEGACY_INPUT_MANAGER
         if (Input.GetKeyDown(KeyCode.C))
         {
             ToggleCameras();
         }
-#else
-        if (Keyboard.current != null && Keyboard.current.cKey.wasPressedThisFrame)
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            ToggleCameras();
+            Jump();
         }
-#endif
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(x, 0f, 0f).normalized * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
+        transform.Rotate(0f, x * rotateSpeed, 0f);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
     }
 
     private void ToggleCameras()
@@ -43,5 +78,30 @@ public class PlayerMovement : MonoBehaviour
         isUsingSecondaryCamera = !isUsingSecondaryCamera;
         playerCamera.enabled = !isUsingSecondaryCamera;
         secondaryCamera.enabled = isUsingSecondaryCamera;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;
     }
 }
